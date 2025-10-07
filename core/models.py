575432422603a8989ea9b2a3f9abe80c78ff3b48
Lucas Meira <lucasrenaa1996@gmail.com>
2025-10-06 23:37:26 -0300

    create model cotacaodolar

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class CotacaoDolar(models.Model):
+    valor = models.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        verbose_name="Valor da Cotação (R$/US$)"
+    )
+
+    data_registro = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Data e Hora do Registro"
+    )
+    fonte = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Fonte da Cotação"
+    )
+
+
+    def __str__(self):
+        return f"Dólar: R${self.valor} em {self.data_registro.strftime('%Y-%m-%d %H:%M')}"
+
+    class Meta:
+        verbose_name = "Cotação do Dólar"
+        verbose_name_plural = "Cotações do Dólar"
+        ordering = ['-data_registro']
